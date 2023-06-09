06955c4fd6d4eb5ced1d5e78e34bafbca0d78c6e
Rakibul-Islam-GitHub <rakibulislam.cse21@gmail.com>
2023-06-09 23:19:03 +0600

    usermodel firstname & lastname is merged to name

--- models/userModel.js
+++ models/userModel.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true },
+        name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
         isAdmin: { type: Boolean, default: false },
@@ -11,6 +10,6 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
